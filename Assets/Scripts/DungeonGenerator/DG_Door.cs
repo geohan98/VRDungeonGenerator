45cb58189a28c1af0169371494885e15dd792141
geohan98 <43745686+geohan98@users.noreply.github.com>
2020-02-19 11:52:22 +0000

    Progress On Editor Tools

--- Assets/Scripts/DungeonGenerator/DG_Door.cs
+++ Assets/Scripts/DungeonGenerator/DG_Door.cs
@@ -7,4 +7,10 @@ public struct DG_Door
 {
     public Vector2Int m_Position;
     public DG_Direction m_Direction;
+
+    public DG_Door(Vector2Int _Position, DG_Direction _Direction)
+    {
+        m_Position = _Position;
+        m_Direction = _Direction;
+    }
 }
--- Assets/Scripts/DungeonGenerator/DG_PaletteDesignerWindow.cs
+++ Assets/Scripts/DungeonGenerator/DG_PaletteDesignerWindow.cs
@@ -5,25 +5,34 @@ using UnityEditor;
 
 public class DG_PaletteDesignerWindow : EditorWindow
 {
+    #region Private Variables
+    private string m_paletteName = "Palette Name";
+    private Object roomPrefab;
+    private Object doorPrefab;
+    #endregion
+
+    #region Unity Functions
+    private void Awake()
+    {
+        if (!(roomPrefab = AssetDatabase.LoadAssetAtPath("Assets/DungeonData/Prefabs/RoomVolume.prefab", typeof(GameObject)))) LogWarning("Failed To Load Room Volume Prefab");
+        if (!(doorPrefab = AssetDatabase.LoadAssetAtPath("Assets/DungeonData/Prefabs/DoorVolume.prefab", typeof(GameObject)))) LogWarning("Failed To Load Door Volume Prefab");
+    }
+    #endregion
+
+    #region Public Functions
     [MenuItem("Dungeon Generator/Palette Designer")]
     public static void ShowWindow()
     {
         GetWindow<DG_PaletteDesignerWindow>("Palette Designer");
     }
+    #endregion
 
-    string m_paletteName;
-    bool m_Debug = true;
-
+    #region Private Functions
     private void OnGUI()
     {
         m_paletteName = GUILayout.TextField(m_paletteName, 128);
 
-        Object roomPrefab;
-        if (!(roomPrefab = AssetDatabase.LoadAssetAtPath("Assets/DungeonData/Prefabs/RoomVolume.prefab", typeof(GameObject)))) LogWarning("Failed To Load Room Volume Prefab");
-        Object doorPrefab;
-        if (!(doorPrefab = AssetDatabase.LoadAssetAtPath("Assets/DungeonData/Prefabs/DoorVolume.prefab", typeof(GameObject)))) LogWarning("Failed To Load Door Volume Prefab");
-
-        if (GUILayout.Button("Create Palette"))
+        if (GUILayout.Button("Create Palette", GUILayout.ExpandWidth(false)))
         {
             if (!AssetDatabase.IsValidFolder("Assets/DungeonData"))
             {
@@ -37,24 +46,37 @@ public class DG_PaletteDesignerWindow : EditorWindow
             AssetDatabase.CreateAsset(new DG_Palette(), "Assets/DungeonData/Palettes/" + m_paletteName + ".asset");
         }
 
-        if (GUILayout.Button("Add Room"))
+        if (GUILayout.Button("Add Room", GUILayout.ExpandWidth(false)))
         {
             Selection.activeObject = Instantiate(roomPrefab);
         }
-        if (GUILayout.Button("Add Door"))
+        if (GUILayout.Button("Add Door", GUILayout.ExpandWidth(false)))
         {
             Selection.activeObject = Instantiate(doorPrefab);
         }
-    }
+        if (GUILayout.Button("Debug Function", GUILayout.ExpandWidth(false)))
+        {
 
+        }
+    }
+    private List<DG_RoomVolume> GetAllRooms()
+    {
+        DG_RoomVolume[] array = FindObjectsOfType<DG_RoomVolume>();
+        List<DG_RoomVolume> list = new List<DG_RoomVolume>();
+        foreach (DG_RoomVolume roomVolume in array)
+        {
+            list.Add(roomVolume);
+        }
+        Log("Found " + list.Count + " Rooms");
+        return list;
+    }
     private void Log(string _msg)
     {
-        if (!m_Debug) return;
-        Debug.Log("[PaletteDesigner][Info]:" + _msg);
+        Debug.Log("[Palette Designer][Info]:" + _msg);
     }
     private void LogWarning(string _msg)
     {
-        if (!m_Debug) return;
-        Debug.Log("[PaletteDesigner][Warning]:" + _msg);
+        Debug.Log("[Palette Designer][Warning]:" + _msg);
     }
+    #endregion
 }
